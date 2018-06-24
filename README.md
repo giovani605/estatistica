7070b3caf7b0b4c22a622ee149e64dab7a744f32
Giovani <giovanibossoni@hotmail.com>
2018-06-24 17:01:36 -0300

    feito exercicio 2

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 Links:
 http://cienciasecognicao.org/portal/wp-content/uploads/2011/09/Tamanho-da-Amostra-1-1.pdf
+
+playlist do me salva
+https://www.youtube.com/watch?v=uNe81VZvqJg&list=PLf1lowbdbFIDbvBg91YxL2haH5Y_lD-Yz
+
+esse canal ajuda muito com R
+https://www.youtube.com/channel/UCaNIxVagLhqupvUiDK01Mgg
--- trabalho1.r
+++ trabalho1.r
@@ -1,18 +1,31 @@
+## Alunos: Giovani Bossoni, Paulo
+
 ## trabalho funções R
-## 1)
+## 1) ## exercicio problematico ainda
 
-## qtd = 380
-# x = numero de favoraveis
-# exercicio errado?
-function exer1(tamanho,x,confianca){
-
-}
-## 
 ## 2)
-## letra a
-function exer2a(confianca,erro){
-  p = 0.5
-  q = 0.5
-  
+## essa funcao resolve o excercio basta escolher os valor p e q
+exer2a  <- function(p,q)
+{
+  Z = 1.65
+  E = 0.02
+  n <- ((Z*Z) * p*q)/((E)*(E))
+  return(n)
   
 }
+## letra a
+## utilize os valores p = 0.5 e q 0.5
+func(0.5,0.5)
+## letra b 
+## utilize os valores p = 0.2 e q 0.8
+func(0.2,0.8)
+
+func(0.5,0.5)
+## P(x <= 70)
+## pnorm(q=70,mean=75,sd=5,lower.tail=T)
+
+
+## calculando propabildiade de Z
+##pnorm(Z< , mean=0, sd = 1)
+
+## t.test
