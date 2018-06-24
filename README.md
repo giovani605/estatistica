fa523c3ad8deb699c9bbe660d30ddfb31ea8907c
Giovani <giovanibossoni@hotmail.com>
2018-06-23 22:31:07 -0300

    primeiro commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # estatistica
+
+Links:
+http://cienciasecognicao.org/portal/wp-content/uploads/2011/09/Tamanho-da-Amostra-1-1.pdf
---
+++ trabalho1.r
@@ -0,0 +1,18 @@
+## trabalho funções R
+## 1)
+
+## qtd = 380
+# x = numero de favoraveis
+# exercicio errado?
+function exer1(tamanho,x,confianca){
+
+}
+## 
+## 2)
+## letra a
+function exer2a(confianca,erro){
+  p = 0.5
+  q = 0.5
+  
+  
+}
